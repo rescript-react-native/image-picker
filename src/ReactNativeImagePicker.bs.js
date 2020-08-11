340ab788ec6c2a77d2d57f9b10bd6182027597a2
Maxime Thirouin <git@moox.io>
2020-08-11 15:10:24 +0200

    Upgrade deps

--- src/ReactNativeImagePicker.bs.js
+++ src/ReactNativeImagePicker.bs.js
@@ -1,11 +1,11 @@
 'use strict';
 
 
-var Button = { };
+var Button = {};
 
-var PermissionDenied = { };
+var PermissionDenied = {};
 
-var $$Storage = { };
+var $$Storage = {};
 
 var Options = {
   Button: Button,
